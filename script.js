58ef811d8d74f164fd56576aa88bd4fbda25e1c5
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2024-03-13 21:04:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+const button = document.getElementById("btn");
+async function showOutput(){
+	const text = document.getElementById("text").value;
+	const delay = document.getElementById("delay").value;
+	setTimeout(()=>{
+		output.innerText = text;
+	},delay)
+}
+
+button.addEventListener("click",showOutput);
+
